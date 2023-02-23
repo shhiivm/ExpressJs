0e3287146acc7ea3bc03d9f799a52d639a8281d3
shhiivm <shhiivm@pm.me>
2023-02-23 08:44:22 +0530

    added partials

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const path = require('path')
 const express = require('express');
+const hbs = require('hbs');
+
 const app = express();
 
 const port = 3001;
 
 
 const staticPath=path.join(__dirname, "/demoWebsite")
-const customPath = path.join(__dirname,"/customView")
+const customPath = path.join(__dirname,"/customView/views")
+const partialsPath = path.join(__dirname,"/customView/partials")
+
+
 //To set the view engine
 app.set('view engine','hbs')
 app.set('views',customPath)
+
+
+hbs.registerPartials(partialsPath)
 
 // app.use(express.static(staticPath))
 
@@ -19,6 +27,10 @@ app.get('/',(req,res)=>{
         AccountName : "Shivam",
     })
 })
+app.get('/about',(req,res)=>{
+    res.render('about')
+})
+
 
 app.get('/',(req,res)=>{
     res.send("Hello from home Page")
